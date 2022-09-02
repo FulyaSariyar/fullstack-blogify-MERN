9e1e2147ba61b53c0daffa31a6412d8235b05bd1
Fulya Sariyar <fulyasariyar@outlook.com>
2022-09-02 10:53:58 +0300

    regulations

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 import cors from 'cors';
 
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({limit: "30mb", extended:true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended:true}))
@@ -20,11 +22,10 @@ app.get("/", (req, res)=>{
 
 const PORT = process.env.PORT || 5000;
 
-const CONNECTION_URL = "mongodb+srv://coderladyfulya:fulya_123@cluster0.wxtdzwb.mongodb.net/?retryWrites=true&w=majority"
-
-mongoose.connect(CONNECTION_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
+mongoose
+   .connect(process.env.CONNECTION_URL,{
+      useNewUrlParser:true,
+      useUnifiedTopology:true
 })
 .then (() => {
   app.listen(PORT, () => {
